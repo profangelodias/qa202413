8bd137565fb17f15587395bfea9e307b2a10b764
Prof. Angelo F. Dias <prof2132@iesp.edu.br>
2024-05-07 21:48:56 -0300

    07/05/2024 - Criamos o 1o teste

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # qa202413
 Repositórios para as aulas de automação de testes com cypress e cucumber (BDD).
+
+Chegou aqui pelo git, digite no terminal:
+
+npm install
---
+++ cypress/e2e/saucedemo/login.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+context('Login do Saucedemo', () => {
+    beforeEach(() => {
+        cy.visit("https://www.saucedemo.com/v1/")
+    });
+    
+    it('Login válido', () => {
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('#login-button').click()
+        cy.get('.product_label').should('have.text',"Products")
+    });
+
+    // it('Login inválido', () => {
+        
+    // });
+
+});
